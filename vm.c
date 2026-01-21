0d3a70257edae478c4aa5b8983ffeadf22176315
NICK SHUM <n4shum@uwaterloo.ca>
2026-01-21 18:44:38 -0500

    added vm stack, debug trace

--- vm.c
+++ vm.c
@@ -1,11 +1,15 @@
 #include "vm.h"
 #include "common.h"
+#include "debug.h"
 #include <stdio.h>
 
 VM vm;
 
 static InterpretResult run();
+static void resetStack();
+
 void initVM() {
+  resetStack();
 }
 void freeVM() {
 }
@@ -22,15 +26,26 @@ static InterpretResult run() {
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()]) // Reads the next byte in the bytecode which pointer to where the constant is in the constant pool, then increments the ip pointer by a byte
 
   for (;;) { // while (true) read and execute a single bytecode instruction
+#ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code)); // disassembleInstruction takes in an offset, so we subtract the current state of IP to the first byte of the code array to find it
+#endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
     case OP_CONSTANT: {
       Value constant = READ_CONSTANT();
-      printValue(constant);
-      printf("\n");
+      push(constant);
       break;
     }
     case OP_RETURN: {
+      printValue(pop());
+      printf("\n");
       return INTERPRET_OK; // exit the loop
     }
     }
@@ -38,4 +53,18 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+}
+
+static void resetStack() {
+  vm.stackTop = vm.stack; // same as &stack[0]
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++; // moves the pointer forward by sizeof(Value) bytes
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
--- vm.h
+++ vm.h
@@ -2,11 +2,17 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct {
   Chunk *chunk;
   uint8_t *ip; // Instruction pointer
   // We use a pointer instaead of an integer index bc its faster to dereference a pointer than look up an element in an array by index
+
+  Value stack[STACK_MAX]; // fixed size for now
+  Value *stackTop;        // stackTop points just past the element containing the top value, can tell if stack is empty if its pointing at element 0 in the array
 } VM;
 
 typedef enum {
@@ -19,5 +25,7 @@ void initVM();
 void freeVM();
 
 InterpretResult interpret(Chunk *chunk);
+void push(Value value);
+Value pop();
 
 #endif
